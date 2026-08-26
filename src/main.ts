f7658f6c66e74d073140b4b512ef02c3a129c779
papimatcoding <matalsan9@gmail.com>
2026-08-26 09:00:39 +0200

    fix: use 2x internal render surface for HiDPI

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
 import Phaser from "phaser";
 import "./styles.css";
+import { DESIGN_HEIGHT, DESIGN_WIDTH, RENDER_SCALE } from "./config/display";
 import { MenuScene } from "./scenes/MenuScene";
 import { LevelSelectScene } from "./scenes/LevelSelectScene";
 import { GameScene } from "./scenes/GameScene";
 import { ResultsScene } from "./scenes/ResultsScene";
 import { CosmeticsScene } from "./scenes/CosmeticsScene";
 
-const resolution = Math.min(window.devicePixelRatio || 1, 2.5);
+const renderWidth = Math.round(DESIGN_WIDTH * RENDER_SCALE);
+const renderHeight = Math.round(DESIGN_HEIGHT * RENDER_SCALE);
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   parent: "game",
-  width: 540,
-  height: 960,
-  resolution,
+  width: renderWidth,
+  height: renderHeight,
   backgroundColor: "#0d1117",
   scene: [MenuScene, LevelSelectScene, GameScene, ResultsScene, CosmeticsScene],
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: 540,
-    height: 960
+    width: renderWidth,
+    height: renderHeight
   },
   render: {
     antialias: true,
