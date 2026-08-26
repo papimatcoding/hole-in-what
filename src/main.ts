423acbe8cda708b02df8457d1b1e8964fa906a94
papimatcoding <matalsan9@gmail.com>
2026-08-26 07:57:29 +0200

    feat: wire Phaser scenes and responsive game config

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import Phaser from "phaser";
+import "./styles.css";
+import { MenuScene } from "./scenes/MenuScene";
+import { LevelSelectScene } from "./scenes/LevelSelectScene";
+import { GameScene } from "./scenes/GameScene";
+import { ResultsScene } from "./scenes/ResultsScene";
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: "game",
+  width: 540,
+  height: 960,
+  backgroundColor: "#0d1117",
+  scene: [MenuScene, LevelSelectScene, GameScene, ResultsScene],
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 540,
+    height: 960
+  },
+  render: {
+    antialias: true,
+    roundPixels: false
+  }
+};
+
+new Phaser.Game(config);
