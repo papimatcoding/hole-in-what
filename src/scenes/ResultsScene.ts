e7b7bb21e88befd5b1a15810a3008da1f827744c
papimatcoding <matalsan9@gmail.com>
2026-08-26 11:34:43 +0200

    fix: use V8.1 objectives on results screen

--- src/scenes/ResultsScene.ts
+++ src/scenes/ResultsScene.ts
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import { setupDesignCamera, sharpenSceneText } from "../config/display";
 import { cosmeticById } from "../data/cosmetics";
 import { levelsForMode } from "../data/levels";
+import { applyV81LevelPatch } from "../data/v81LevelPatches";
 import { SaveSystem } from "../systems/SaveSystem";
 import { formatRequirement, requirementMet } from "../systems/StarScoring";
 import type { ResultsSceneData } from "../types";
@@ -20,7 +21,7 @@ export class ResultsScene extends Phaser.Scene {
   create(): void {
     setupDesignCamera(this);
     this.cameras.main.setBackgroundColor("#0b0f14");
-    const levels = levelsForMode(this.resultData.mode);
+    const levels = levelsForMode(this.resultData.mode).map(applyV81LevelPatch);
     const level = levels[this.resultData.levelIndex];
     const reward = SaveSystem.submit(
       this.resultData.levelId,
