63b06d11ffd3b5b98ca0db7e3e5a7a861f886ece
papimatcoding <matalsan9@gmail.com>
2026-08-27 08:08:49 +0200

    editor: register level preview browser

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { CosmeticsScene } from "./scenes/CosmeticsScene";
 import { ShopScene } from "./scenes/ShopScene";
 import { RewardsScene } from "./scenes/RewardsScene";
 import { EditorScene } from "./scenes/EditorScene";
+import { LevelPreviewScene } from "./scenes/LevelPreviewScene";
 
 const renderWidth=Math.round(DESIGN_WIDTH*RENDER_SCALE);
 const renderHeight=Math.round(DESIGN_HEIGHT*RENDER_SCALE);
@@ -19,7 +20,7 @@ const config:Phaser.Types.Core.GameConfig={
   width:renderWidth,
   height:renderHeight,
   backgroundColor:"#0d1117",
-  scene:[MenuScene,LevelSelectScene,GameplayScene,ResultsScene,CosmeticsScene,ShopScene,RewardsScene,EditorScene],
+  scene:[MenuScene,LevelSelectScene,GameplayScene,ResultsScene,CosmeticsScene,ShopScene,RewardsScene,EditorScene,LevelPreviewScene],
   scale:{mode:Phaser.Scale.FIT,autoCenter:Phaser.Scale.CENTER_BOTH,width:renderWidth,height:renderHeight},
   render:{antialias:true,roundPixels:false}
 };
