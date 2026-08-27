8866bf26c2f3336e77a6ffb42e0d38a4491ece6c
papimatcoding <matalsan9@gmail.com>
2026-08-27 08:11:30 +0200

    cleanup: remove obsolete procedural HARD wrapper

--- src/data/procedural/hardCourses.ts
+++
@@ -1,6 +0,0 @@
-import type { LevelDefinition } from "../../types";
-import { buildCampaignCourse } from "./auditedCampaign";
-
-export function buildHardCourse(index:number):LevelDefinition {
-  return buildCampaignCourse("troll",index);
-}
