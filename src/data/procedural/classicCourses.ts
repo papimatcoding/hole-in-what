1ad46431475fc96ac926a3ce5208212fa01ed225
papimatcoding <matalsan9@gmail.com>
2026-08-27 08:11:21 +0200

    cleanup: remove obsolete procedural Classic wrapper

--- src/data/procedural/classicCourses.ts
+++
@@ -1,6 +0,0 @@
-import type { LevelDefinition } from "../../types";
-import { buildCampaignCourse } from "./auditedCampaign";
-
-export function buildClassicCourse(index:number):LevelDefinition {
-  return buildCampaignCourse("classic",index);
-}
