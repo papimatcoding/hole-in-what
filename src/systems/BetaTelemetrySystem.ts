ac4255f2a80a7b77d286f03e85d3661ffe7a44b8
papimatcoding <matalsan9@gmail.com>
2026-08-27 12:40:00 +0200

    beta: start friends RC2 telemetry bucket

--- src/systems/BetaTelemetrySystem.ts
+++ src/systems/BetaTelemetrySystem.ts
@@ -1,7 +1,7 @@
 import type { GameMode } from "../types";
 
 const ENDPOINT="https://xtekdrkqgfjnnwawyoim.supabase.co/functions/v1/beta-feedback";
-export const BETA_BUILD_ID="beta-block-1-friends-rc1";
+export const BETA_BUILD_ID="beta-block-1-friends-rc2";
 const TESTER_KEY="troll-golf-beta-tester-id-v1";
 const ALIAS_KEY="troll-golf-beta-tester-alias-v1";
 const ASKED_ALIAS_KEY="troll-golf-beta-tester-alias-asked-v1";
